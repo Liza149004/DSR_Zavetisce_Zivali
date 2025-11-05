d643edcd303f05083f97c9ec195fb370e1e2d0b0
Liza149004 <liza.stupar@student.um.si>
2025-11-05 13:23:50 +0100

    set up

---
+++ data/www/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <h1>DELAAAAAA!!!</h1>
+    </head>    
+    <body>
+        <p>To je body test</p>
+    </body>
+    <footer>
+        <p>To pa footer test</p>
+        <p>Vse dela :D</p>
+    </footer>
+</html>
